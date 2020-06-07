225be1cedd945df94e805c636e1ed805d8532465
pradeep-charism <pradeep.charism@gmail.com>
2020-06-08 00:35:25 +0800

    react-boot

---
+++ react-boot/src/test/java/com/myapp/reactboot/ReactBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.myapp.reactboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReactBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
