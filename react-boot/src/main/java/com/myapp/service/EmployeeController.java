95166f088f972e92d3c7a4c8e165a3cf3254fc84
pradeep-charism <pradeep.charism@gmail.com>
2020-06-08 02:02:27 +0800

    integrate with oracle

--- react-boot/src/main/java/com/myapp/service/EmployeeController.java
+++ react-boot/src/main/java/com/myapp/service/EmployeeController.java
@@ -8,37 +8,38 @@ import java.util.List;
 
 @RequestMapping("/api")
 public class EmployeeController {
- 
- @Autowired
- private EmployeeService employeeService;
- 
- @GetMapping("/employee")
- public List<Employee> get() {
-  return employeeService.get();
- }
- 
- @PostMapping("/employee")
- public Employee save(@RequestBody Employee employee) {
-  employeeService.save(employee);
-  return employee;
- }
- 
- @GetMapping("/employee/{id}")
- public Employee get(@PathVariable int id) {
-  return employeeService.get(id);
- }
- 
- @DeleteMapping("/employee/{id}")
- public String delete(@PathVariable int id) {
-  
-  employeeService.delete(id);
-  
-  return "Employee removed with id "+id;
-  
- }
- 
- @PutMapping("/employee")
- public Employee update(@RequestBody Employee employee) {
-  employeeService.save(employee);
-  return employee;
- }}
+
+    @Autowired
+    private EmployeeService employeeService;
+
+    @GetMapping("/employee")
+    public List<Employee> get() {
+        return employeeService.get();
+    }
+
+    @PostMapping("/employee")
+    public Employee save(@RequestBody Employee employee) {
+        employeeService.save(employee);
+        return employee;
+    }
+
+    @GetMapping("/employee/{id}")
+    public Employee get(@PathVariable int id) {
+        return employeeService.get(id);
+    }
+
+    @DeleteMapping("/employee/{id}")
+    public String delete(@PathVariable int id) {
+
+        employeeService.delete(id);
+
+        return "Employee removed with id " + id;
+
+    }
+
+    @PutMapping("/employee")
+    public Employee update(@RequestBody Employee employee) {
+        employeeService.save(employee);
+        return employee;
+    }
+}
