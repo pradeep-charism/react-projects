f8b86c9087d67d53e816c6898f34d7416c1aae2d
pradeep-charism <pradeep.charism@gmail.com>
2020-06-09 21:08:49 +0800

    full crud program

--- stock-app/stock-app-backend/src/main/java/com/my/stockapp/stock/StockRepository.java
+++ stock-app/stock-app-backend/src/main/java/com/my/stockapp/stock/StockRepository.java
@@ -1,81 +1,74 @@
 package com.my.stockapp.stock;
 
-import java.util.ArrayList;
-import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
 import org.springframework.stereotype.Repository;
-import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
+import java.util.ArrayList;
+import java.util.List;
+
+@Transactional
 @Repository
 public class StockRepository {
 
-	private static List<Stock> stocks = new ArrayList<>();
-	private static long idCounter = 0;
+    private static List<Stock> stocks = new ArrayList<>();
+    private static long idCounter = 0;
 
-	private static final String INVESTOR = "sg-investor";
+    private static final String INVESTOR = "sg-investor";
 
-	@Autowired
-	JdbcTemplate jdbcTemplate;
+    @Autowired
+    JdbcTemplate jdbcTemplate;
 
-	@Autowired
-	private NamedParameterJdbcTemplate namedParameterJdbcTemplate;
+    @Autowired
+    private NamedParameterJdbcTemplate namedParameterJdbcTemplate;
 
+//	static {
+//		stocks.add(new Stock(1, INVESTOR, "Singtel."));
+//		stocks.add(new Stock(2, INVESTOR, "City Developments."));
+//		stocks.add(new Stock(3, INVESTOR, "SBS Transit."));
+//		stocks.add(new Stock(4, INVESTOR, "SATS Ltd."));
+//	}
 
-	static {
-		stocks.add(new Stock(1, INVESTOR, "Singtel."));
-		stocks.add(new Stock(2, INVESTOR, "City Developments."));
-		stocks.add(new Stock(3, INVESTOR, "SBS Transit."));
-		stocks.add(new Stock(4, INVESTOR, "SATS Ltd."));
-	}
+    public List<Stock> findAll() {
+        stocks = jdbcTemplate.query(
+                "select * from portfolio",
+                (rs, rowNum) ->
+                        new Stock(
+                                rs.getLong("id"),
+                                rs.getString("holder"),
+                                rs.getString("stock_name")
+                        )
+        );
+        idCounter = stocks.size();
+        return stocks;
+    }
 
-	public List<Stock> findAll() {
-		return jdbcTemplate.query(
-				"select * from portfolio",
-				(rs, rowNum) ->
-						new Stock(
-								rs.getLong("id"),
-								rs.getString("holder"),
-								rs.getString("stock_name")
-						)
-		);
-	}
+    public Stock save(Stock stock) {
+        int isUpdated = jdbcTemplate.update(
+                "insert into portfolio (id, holder, stock_name) values(?, ?, ?)",
+                ++idCounter, INVESTOR, stock.getDescription());
 
+        return stock;
+    }
 
+    public Stock deleteById(long id) {
+        Stock stock = findById(id);
+        int isUpdated = jdbcTemplate.update("delete from portfolio where id = ?", id);
+        return stock;
+    }
 
-	public Stock save(Stock stock) {
-		if (stock.getId() == -1 || stock.getId() == 0) {
-			stock.setId(++idCounter);
-			stocks.add(stock);
-		} else {
-			deleteById(stock.getId());
-			stocks.add(stock);
-		}
-		return stock;
-	}
-
-	public Stock deleteById(long id) {
-		Stock stock = findById(id);
-
-		if (stock == null)
-			return null;
-
-		if (stocks.remove(stock)) {
-			return stock;
-		}
-
-		return null;
-	}
-
-	public Stock findById(long id) {
-		for (Stock stock : stocks) {
-			if (stock.getId() == id) {
-				return stock;
-			}
-		}
-
-		return null;
-	}
+    public Stock findById(long id) {
+        return jdbcTemplate.queryForObject(
+                "select * from portfolio where id = ?",
+                new Object[]{id},
+                (rs, rowNum) ->
+                        new Stock(
+                                rs.getLong("id"),
+                                rs.getString("holder"),
+                                rs.getString("stock_name")
+                        )
+        );
+    }
 }
