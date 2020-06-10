f7c0a3146f2de08bbfe5a6a45f5df0c140078cf8
pradeep-charism <pradeep.charism@gmail.com>
2020-06-10 16:37:39 +0800

    Read form inputs

---
+++ learn-react/src/ReadFormInputComponent.js
@@ -0,0 +1,70 @@
+import React, { useState } from 'react';
+import './App.css';
+import './card_list.css';
+
+const testData = [
+  { name: "Dan Abramov", avatar_url: "https://avatars0.githubusercontent.com/u/810438?v=4", company: "@facebook" },
+  { name: "Sophie Alpert", avatar_url: "https://avatars2.githubusercontent.com/u/6820?v=4", company: "Humu" },
+  { name: "Sebastian Markbåge", avatar_url: "https://avatars2.githubusercontent.com/u/63648?v=4", company: "Facebook" },
+];
+
+const CardList = (props) => (
+  <div>
+    {testData.map(profile => <Card {...profile} />)}
+  </div>
+);
+
+class Form extends React.Component {
+  state = { userName: '' };
+  handleSubmit = (event) => {
+    event.preventDefault();
+    console.log(this.state.userName);
+  };
+  render() {
+    return (
+      <form onSubmit={this.handleSubmit}>
+        <input
+          type="text"
+          value={this.state.userName}
+          onChange={event => this.setState({ userName: event.target.value })}
+          placeholder="GitHub username"
+          required
+        />
+        <button>Add card</button>
+      </form>
+    );
+  }
+}
+
+class Card extends React.Component {
+  render() {
+    const profile = this.props;
+    return (
+      <div className="github-profile">
+        <img className="img" src={profile.avatar_url} />
+        <div className="info">
+          <div className="name">{profile.name}</div>
+          <div className="company">{profile.company}</div>
+        </div>
+      </div>
+    );
+  }
+}
+
+class App extends React.Component {
+  state = {
+    profiles: testData,
+  };
+
+  render() {
+    return (
+      <div>
+        <div className="header">{this.props.title}</div>
+        <Form />
+        <CardList profiles={this.state.profiles} />
+      </div>
+    );
+  }
+}
+
+export default App;
