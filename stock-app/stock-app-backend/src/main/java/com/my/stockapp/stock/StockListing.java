602f129b89decdf7f5d596ba966a0c2fa3eb3ac8
pradeep-charism <pradeep.charism@gmail.com>
2020-06-11 12:07:46 +0800

    building json exchange

--- stock-app/stock-app-backend/src/main/java/com/my/stockapp/stock/StockListing.java
+++ stock-app/stock-app-backend/src/main/java/com/my/stockapp/stock/StockListing.java
@@ -5,15 +5,9 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 @CrossOrigin(origins = { "http://localhost:3000", "http://localhost:4200" })
@@ -31,6 +25,58 @@ public class StockListing {
 	@GetMapping("/depository/{username}/stocks/{id}")
 	public Stock getStock(@PathVariable String username, @PathVariable long id) {
 		return stockRepository.findById(id);
+	}
+
+	@CrossOrigin
+	@ResponseBody
+	@RequestMapping(value="/depository/github/data/", method=RequestMethod.POST, produces = MediaType.APPLICATION_JSON_VALUE)
+	public String getAllRecords(@PathVariable String userName) {
+		System.out.println("inside get data" + userName);
+		List<Stock> all = stockRepository.findAll();
+		
+		return "data";
+	}
+
+	@CrossOrigin
+	@ResponseBody
+	@RequestMapping(value="/depository/github/data/{userName}",method=RequestMethod.POST, produces = MediaType.APPLICATION_JSON_VALUE)
+	public String getData(@PathVariable String userName) {
+		System.out.println("inside get data" + userName);
+		String data = "{\n" +
+				"  \"login\": \"pradeep-charism\",\n" +
+				"  \"id\": 20122097,\n" +
+				"  \"node_id\": \"MDQ6VXNlcjIwMTIyMDk3\",\n" +
+				"  \"avatar_url\": \"https://avatars2.githubusercontent.com/u/20122097?v=4\",\n" +
+				"  \"gravatar_id\": \"\",\n" +
+				"  \"url\": \"https://api.github.com/users/pradeep-charism\",\n" +
+				"  \"html_url\": \"https://github.com/pradeep-charism\",\n" +
+				"  \"followers_url\": \"https://api.github.com/users/pradeep-charism/followers\",\n" +
+				"  \"following_url\": \"https://api.github.com/users/pradeep-charism/following{/other_user}\",\n" +
+				"  \"gists_url\": \"https://api.github.com/users/pradeep-charism/gists{/gist_id}\",\n" +
+				"  \"starred_url\": \"https://api.github.com/users/pradeep-charism/starred{/owner}{/repo}\",\n" +
+				"  \"subscriptions_url\": \"https://api.github.com/users/pradeep-charism/subscriptions\",\n" +
+				"  \"organizations_url\": \"https://api.github.com/users/pradeep-charism/orgs\",\n" +
+				"  \"repos_url\": \"https://api.github.com/users/pradeep-charism/repos\",\n" +
+				"  \"events_url\": \"https://api.github.com/users/pradeep-charism/events{/privacy}\",\n" +
+				"  \"received_events_url\": \"https://api.github.com/users/pradeep-charism/received_events\",\n" +
+				"  \"type\": \"User\",\n" +
+				"  \"site_admin\": false,\n" +
+				"  \"name\": \"Pradeep Kumar\",\n" +
+				"  \"company\": \"Charism Corp\",\n" +
+				"  \"blog\": \"\",\n" +
+				"  \"location\": \"Singapore\",\n" +
+				"  \"email\": null,\n" +
+				"  \"hireable\": true,\n" +
+				"  \"bio\": \"Ask me :)\",\n" +
+				"  \"twitter_username\": null,\n" +
+				"  \"public_repos\": 25,\n" +
+				"  \"public_gists\": 0,\n" +
+				"  \"followers\": 2,\n" +
+				"  \"following\": 3,\n" +
+				"  \"created_at\": \"2016-06-24T05:39:37Z\",\n" +
+				"  \"updated_at\": \"2020-06-10T11:03:47Z\"\n" +
+				"}\n";
+		return data;
 	}
 
 	@DeleteMapping("/depository/{username}/stocks/{id}")
