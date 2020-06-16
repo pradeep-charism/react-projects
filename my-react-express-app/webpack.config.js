9a191255e357de702f948527274ecf43f64ec6ad
pradeep-charism <pradeep.charism@gmail.com>
2020-06-16 19:38:59 +0800

    checkin mandatory scripts

--- my-react-express-app/webpack.config.js
+++ my-react-express-app/webpack.config.js
@@ -7,6 +7,20 @@ module.exports = {
         path: path.resolve(__dirname, 'dist'),
         filename: 'bundle.js',
         publicPath: '/'
+    },
+    resolve: {
+        extensions: ['.js', 'jsx']
+    },
+    devServer: {
+        historyApiFallback: true
+    },
+    module: {
+        rules: [
+            {
+            test: /\.jsx?/,
+            loader: 'babel-loader'
+        }
+    ]
     }
 
 }
