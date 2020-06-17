96b4a32e5454a360655547fd48dadf2bae0801bd
pradeep-charism <pradeep.charism@gmail.com>
2020-06-17 10:48:19 +0800

    create stock app using webpack

---
+++ my-react-express-app/src/app/components/Dashboard.jsx
@@ -0,0 +1,15 @@
+import { connect } from 'react-redux';
+import React from 'react';
+import { ConnectedTaskList } from './TaskList';
+
+const Dashboard = ({groups})=>(
+    <div className="row">
+        {groups.map(group=>(
+            <ConnectedTaskList key={group.id} {...group} className="col"/>
+        ))}
+    </div>
+);
+
+const mapStateToProps = ({groups})=>({groups});
+
+export const ConnectedDashboard = connect(mapStateToProps)(Dashboard);
---
+++ stock-app/stock-app-webpack-ui/webpack.config.js
@@ -0,0 +1,26 @@
+const path = require("path");
+
+module.exports = {
+    mode: "development",
+    entry: path.resolve(__dirname, "src"),
+    output: {
+        path: path.resolve(__dirname, 'build'),
+        filename: 'bundle.js',
+        publicPath: '/'
+    },
+    resolve: {
+        extensions: ['.js', 'jsx']
+    },
+    devServer: {
+        historyApiFallback: true
+    },
+    module: {
+        rules: [
+            {
+            test: /\.jsx?/,
+            loader: 'babel-loader'
+        }
+    ]
+    }
+
+}
