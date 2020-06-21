a9e896d38551832eff4ebdc98a687609edccabe0
pradeep-charism <pradeep.charism@gmail.com>
2020-06-21 18:18:37 +0800

    updated

--- react-redux-tutorial/src/components/courses/CoursesPage.js
+++ react-redux-tutorial/src/components/courses/CoursesPage.js
@@ -2,8 +2,32 @@ import React from "react";
 import { render } from "react-dom";
 
 class CoursesPage extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      courses: {
+        title: "",
+      },
+    };
+  }
+
+  handleChange(event) {
+    const course = { ...this.state.course, title: event.target.value };
+    this.setState({ course: course });
+  }
+
   render() {
-    return <h1>Courses Page</h1>;
+    return (
+      <form>
+        <h2>Course</h2>
+        <input
+          type="text"
+          onChange={this.handleChange}
+          value={this.state.courses.title}
+        />
+        <input type="submit" value="Save" />
+      </form>
+    );
   }
 }
 
